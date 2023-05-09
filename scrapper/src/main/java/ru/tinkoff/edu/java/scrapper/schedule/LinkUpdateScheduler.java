ff646a80afbe955b8f32f63e6594d5120200de7d
vbandurin <v.bandurin.w@mail.ru>
2023-05-09 21:13:48 +0300

    scrapper change

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/schedule/LinkUpdateScheduler.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/schedule/LinkUpdateScheduler.java
@@ -16,6 +16,7 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class LinkUpdateScheduler {
+
     private final LinkService linkService;
     private final UpdateHandler updateHandler;
 
