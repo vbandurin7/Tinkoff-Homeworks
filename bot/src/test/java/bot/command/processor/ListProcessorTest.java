a0621fb24ef3c7469224250f2ec5313e3d489283
vbandurin <v.bandurin.w@mail.ru>
2023-05-10 09:20:54 +0300

    bot change

--- bot/src/test/java/bot/command/processor/ListProcessorTest.java
+++ bot/src/test/java/bot/command/processor/ListProcessorTest.java
@@ -27,7 +27,7 @@ import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
-public class ListProcessorTest {
+class ListProcessorTest {
 
     @Mock
     private CommandValidatorImpl commandValidator;
